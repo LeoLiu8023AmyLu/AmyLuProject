3b579b5104d03a4f4529293468dd5c4acb0cbd5b
LeoLiu8023AmyLu <792493310@qq.com>
2017-04-19 17:40:09 +0800

    更新项目代码

--- AmyLu_Matlab_Project/AmyLuMatlabProject.m
+++ AmyLu_Matlab_Project/AmyLuMatlabProject.m
@@ -2,22 +2,22 @@ clc;
 close all;
 clear all;
 PTB_Flag=0; % 1 为打开 PTB 0 为 关闭 (调试用，在PTB不正常的情况下 调试其他功能)
-VolunteerName='AmyLuA';
+VolunteerName='AmyLuA'; 
 % 数据初始化
 FolderPath='D:\workspace\AmyLuProject\AmyLu_Matlab_Project\';	% 变更文件地址
 Excel_DATA_FileName = [FolderPath,'DATA.xls'];  % 得到Excel电子表格完整目录
 Excel_OUTPUT_FileName = [FolderPath,'OutPut.xls'];  % 得到Excel电子表格完整目录
 [NUM,TXT,RAW]=xlsread(Excel_DATA_FileName ,1,'A2:C201');  % 获得表格中的数据
-Video_Name_C=RAW; % 文件交换 Video_Name_C(行号,列号) 
+Video_Name_C=RAW; % 文件交换 Video_Name_C(行号,列号) 引索由1开始
 % Video_Name_C(N,1) 序号
 % Video_Name_C(N,2) 文件名
 % Video_Name_C(N,3) 车牌号
-Random_Series=randperm(length(Video_Name_C));   %生成随机数列
-OutPut_Cell={};
+Random_Series=randperm(length(Video_Name_C));   % 生成随机数列
+OutPut_Cell={}; % 输出的初始化
 % PTB工具初始化
 if(PTB_Flag==1)
     Screen('Preference','TextEncodingLocale','UTF-8');  % 文本显示编码用 utf-8
-    Screen('Preference', 'SkipSyncTests', 0)    % 跳过检查
+    Screen('Preference', 'SkipSyncTests', 1)    % 跳过检查
     screenGrps=Screen('Screens');   % 初始化 屏幕
     screenid=max(screenGrps);   % 全屏显示
     [win,rect] = Screen('OpenWindow', screenid,0);  % 
@@ -36,7 +36,7 @@ for n=1:length(Video_Name_C)    % 设置循环
        Screen('PlayMovie',ssss, 1);
     end
     %选择答案
-       Temp_Anwser=unidrnd(2)-1;    % 目前先用随机数生成 答案对错
+    Temp_Anwser=unidrnd(2)-1;    % 目前先用随机数生成 答案对错
     %记录函数
     OutPut_Cell(n,1)=num2cell(n);   %记录序号
     OutPut_Cell(n,2)=Temp_Number;   %记录原始序号
--- README.md
+++ README.md
@@ -7,9 +7,15 @@
 2.选择车牌作答 程序返回1或者0(1为正确值，0为错误值)，程序主要利用PTB工具进行选择</br>
 3.采集脑电信号需要了解清楚，之后想办法融合到Matlab中去</br>
 
+2017.04.19 为亲爱的讲解函数,查找 PTB 错误解决方法</br>
+
+
 ## 相关资料：</br>
 关于 PTB Screen 函数的一些网址：</br>
 [1. PTB OpenMovie 函数说明 http://docs.psychtoolbox.org/OpenMovie](http://docs.psychtoolbox.org/OpenMovie)</br>
 [2. PTB PlayMovie 函数说明 http://docs.psychtoolbox.org/PlayMovie](http://docs.psychtoolbox.org/PlayMovie)</br>
 [3. PTB Screen 函数说明 http://docs.psychtoolbox.org/Screen](http://docs.psychtoolbox.org/Screen)</br>
 [4. PTB FAQ https://github.com/Psychtoolbox-3/Psychtoolbox-3/wiki/FAQ](https://github.com/Psychtoolbox-3/Psychtoolbox-3/wiki/FAQ)</br>
+[5. PTB 官网  http://psychtoolbox.org/](http://psychtoolbox.org/)</br>
+[6. PTB Demo例子 http://peterscarfe.com/ptbtutorials.html](http://peterscarfe.com/ptbtutorials.html)</br>
+[7. PTB Text 例子程序 http://peterscarfe.com/textdemo.html](http://peterscarfe.com/textdemo.html)</br>
