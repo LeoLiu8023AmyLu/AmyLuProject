31248204dbcea4253a5838c7af424db96a90f52c
LeoLiu8023AmyLu <792493310@qq.com>
2017-05-09 11:53:02 +0800

    修改Python程序，加入txt生成部分，随机生成 40个车牌的 文本文件，加入Ps模板

--- AmyLu_Matlab_Project/Random_Car_Code_Generator.py
+++ AmyLu_Matlab_Project/Random_Car_Code_Generator.py
@@ -4,6 +4,8 @@ Created on Mon May  8 16:03:43 2017
 @author: Anshare_LY
 """
 import random
+import time
+import codecs
 
 def Random_Car_Code_Generator(NumOfText,city):
     Car_City=''.join(random.sample(city,1))
@@ -20,6 +22,19 @@ def Random_Car_Code_Generator(NumOfText,city):
     print("最终车牌:  "+Car_Code)
     return Car_Code
 
+def OutPutCarCodeList(Car_Code_List, num ,title):
+    try:
+        FileName="CarCode_"+str(num)+" "+(time.strftime('%Y%m%d',time.localtime(time.time())))+".txt"
+        TextFile=codecs.open(FileName,"w","utf-8")
+        TextFile.write(u""+title+"\n")
+        for i in range(num):
+            CarCode_Temp=Car_Code_List[i]
+            TextFile.write(u""+str(CarCode_Temp)+"\n")
+        TextFile.close()
+        print("--> 文件保存成功! 文件名: "+FileName+"\n--> 车牌总数: %d个"%(num))
+    except:
+        print("\n-!-> 文件输出函数错误!")
+
 def main():
     Num_Of_Text=2
     Num_Of_Car_Code=40
@@ -27,6 +42,6 @@ def main():
     Car_Code=[]
     for x in range(Num_Of_Car_Code):
         Car_Code.append(Random_Car_Code_Generator(Num_Of_Text,city))
-    #print(Car_Code)
-    
+    OutPutCarCodeList(Car_Code, len(Car_Code) ,"CarCode")
+
 main()
