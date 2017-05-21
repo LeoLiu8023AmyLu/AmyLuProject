78af5662456ab90b9130f51f18788ab42d96985d
LeoLiu8023AmyLu <792493310@qq.com>
2017-05-21 21:50:38 +0800

    修改细节

--- AmyLu_Matlab_Project/归档文件/PTB_Text1.m
+++ AmyLu_Matlab_Project/归档文件/PTB_Text1.m
@@ -34,8 +34,7 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
 % Draw text in the upper portion of the screen with the default font in red
 %Screen('TextSize', window, 70);
-%DrawFormattedText(window, 'Hello World', 'center',...
-    screenYpixels * 0.25, [1 0 0]);
+%DrawFormattedText(window, 'Hello World', 'center',screenYpixels * 0.25, [1 0 0]);
 
 % Draw text in the middle of the screen in Courier in white
 %Screen('TextSize', window, 80);
@@ -45,8 +44,7 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 % Draw text in the bottom of the screen in Times in blue
 %Screen('TextSize', window, 90);
 %Screen('TextFont', window, 'Times');
-%DrawFormattedText(window, 'Hello World', 'center',...
-    screenYpixels * 0.75, [0 0 1]);
+%DrawFormattedText(window, 'Hello World', 'center',screenYpixels * 0.75, [0 0 1]);
 AnswerNum=3;
 % 数据初始化
 FolderPath='E:\AmyLu_Matlab_Project\';	% 变更文件地址
